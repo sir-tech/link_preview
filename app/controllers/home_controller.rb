bd04c07d1309a08b9b738a97171a805ed4c981b6
Sirko Sittig <sirko.sittig@gmail.com>
2019-06-14 15:23:20 -0300

    add image dimension endpoint

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def image_dimension
+    if params[:url]
+      @image_dimension = FastImage.size("http://upload.wikimedia.org/wikipedia/commons/b/b4/Mardin_1350660_1350692_33_images.jpg", raise_on_failure: false, timeout: 2.0)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'image_dimension', to: 'home#image_dimension'
+  root to: 'home#index'
 end
