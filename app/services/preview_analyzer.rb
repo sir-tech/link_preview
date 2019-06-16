97ae8c76c6e29dd032a26aa7abf55ed3f66664d6
Sirko Sittig <sirko.sittig@gmail.com>
2019-06-16 01:13:21 -0300

    quickfix for facebook sharing hint

--- app/services/preview_analyzer.rb
+++ app/services/preview_analyzer.rb
@@ -1,5 +1,5 @@
 class PreviewAnalyzer
-  attr_accessor :metatags, :og_tags, :whatsapp_sharing_tags
+  attr_accessor :metatags, :og_tags, :whatsapp_sharing_tags, :facebook_sharing_tags
 
   def initialize(url:)
     begin
@@ -36,13 +36,29 @@ class PreviewAnalyzer
     @whatsapp_sharing_tags ||= {}
     @whatsapp_sharing_tags[:small_images] ||= {}
     og_images.each do |image_url|
-      _image_hint = image_hint(get_image_resolution(image_url))
+      _image_hint = wa_image_hint(get_image_resolution(image_url))
       @whatsapp_sharing_tags[:small_images][image_url] = _image_hint if _image_hint
     end
     @whatsapp_sharing_tags
   end
 
-  def image_hint(resolution)
+  def wa_image_hint(resolution)
+    return unless resolution
+    return 'Image too small for sharing' if resolution[0] < 100 || resolution[1] < 100
+  end
+
+  def facebook_sharing_tags
+    return @facebook_sharing_tags if @facebook_sharing_tags
+    @facebook_sharing_tags ||= {}
+    @facebook_sharing_tags[:small_images] ||= {}
+    og_images.each do |image_url|
+      _image_hint = fb_image_hint(get_image_resolution(image_url))
+      @facebook_sharing_tags[:small_images][image_url] = _image_hint if _image_hint
+    end
+    @facebook_sharing_tags
+  end
+
+  def fb_image_hint(resolution)
     return unless resolution
     return 'Image too small for sharing' if resolution[0] < 200 || resolution[1] < 200
   end
